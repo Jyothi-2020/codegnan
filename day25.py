e5ab91c418c9bcc7ac1aa221f54fbcd7b40c2c47
Jyothi Kotyada <kotyadajyothi3@gmail.com>
2026-06-17 10:36:07 +0530

    Add files via upload

---
+++ day25.py
@@ -0,0 +1,105 @@
+'''
+Matplotlib
+----------
+->This is a library
+
+->Grid
+->Title
+->Legend
+
+
+import matplotlib.pyplot as plt
+Sales = ['A','B','C']
+Values =[25,30,45]
+plt.bar(Sales,Values,color='yellow',edgecolor='black')
+plt.xlabel('comapny models')
+plt.ylabel('Values')
+plt.title('BMW Sales')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+overs=[1,2,3,4,5]
+score=[4,9,17,20,8]
+plt.plot(overs,score,color='yellow')
+plt.title('score card')
+plt.xlabel('overs')
+plt.ylable('score')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+subjects=['pyhton','java','data analytics','c']
+students=[35,7,15,9]
+plt.pie(students,labels=subjects)
+plt.legend(subjects)
+plt.title('students in courses')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+subjects=['pyhton','java','data analytics','c']
+students=[35,7,15,9]
+plt.pie(students,labels=subjectsautopct='%1.1f%%')
+plt.legend(subjects)
+plt.title('students in courses')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+
+y=[10,15,18,20,13]
+plt.scatter(x,y)
+plt.title('scatter plot')
+plt.xlabel('X values')
+plt.ylabel('Y values')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+y=[10,18,32,45]
+plt.hist(y)
+plt.title('Histogram plot')
+plt.xlabel('X values')
+plt.ylabel('Y values')
+plt.show()
+
+
+import matplotlib.pyplot as plt
+Sales = ['A','B','C']
+Values =[25,30,45]
+plt.bar(Sales,Values,color='yellow',edgecolor='black')
+plt.xlabel('comapny models')
+plt.ylabel('Values')
+plt.title('BMW Sales')
+plt.show()
+'''
+    
+import matplotlib.pyplot as plt
+Years=['ab','bc','cd']
+Profit=[12,45,34]
+plt.bar(Years,Profit,color='orange',edgecolor='blue')
+plt.xlabel('company')
+plt.ylabel('profit')
+plt.title('vihaan')
+plt.show()
+
+
+
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
